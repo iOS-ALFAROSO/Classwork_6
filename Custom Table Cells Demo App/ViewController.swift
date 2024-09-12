88aea40f3b4649436cba1f01b4a492c05369f700
Alberth Rodriguez <alberth.sotelo@gmail.com>
2024-09-11 20:46:19 -0700

    Initial Commit

---
+++ Custom Table Cells Demo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Table Cells Demo App
+//
+//  Created by Alberth Rodriguez on 9/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
